bed57c8fb4405941f556b657f771e493369e2959
leopittelli <leo6987@gmail.com>
2018-10-03 22:33:10 -0300

    wip styles + wasm test

--- public/chat-frontend.js
+++ public/chat-frontend.js
@@ -1,137 +1,170 @@
-// open connection
-var connection = new WebSocket('ws://127.0.0.1:1337');
+const chat = (function () {
+    let connection;
+    let myName = false;
+    const content = $('#content');
+    const inner = $('#inner');
+    const input = $('#input');
+    const sendButton = $('#send');
 
-$(function () {
-    "use strict";
-
-    // for better performance - to avoid searching in DOM
-    var content = $('#content');
-    var input = $('#input');
-    var status = $('#status');
-
-    // my color assigned by the server
-    var myColor = false;
-    // my name sent to the server
-    var myName = false;
-
-    // if user is running mozilla then use it's built-in WebSocket
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
-
-    // if browser doesn't support WebSocket, just show some notification and exit
-    if (!window.WebSocket) {
-        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
-                                    + 'support WebSockets.'} ));
-        input.hide();
-        $('span').hide();
-        return;
+    function scrollBottom() {
+        inner[0].scrollTop = $(content).outerHeight(true);
     }
 
-    connection.onopen = function () {
-        // first we want users to enter their names
-        input.removeAttr('disabled');
-        status.text('Choose name:');
-    };
+    function onRecieve(message, who="them") {
+        content.append(buildHTML(message, who));
 
-    connection.onerror = function (error) {
-        // just in there were some problems with conenction...
-        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
-                                    + 'connection or the server is down.' } ));
-    };
+        scrollBottom();
+    }
+    function onSend(message) {
+        console.log(message)
+        content.append(buildHTML(message, "me"));
 
-    // most important part - incoming messages
-    connection.onmessage = function (message) {
-        // try to parse JSON message. Because we know that the server always returns
-        // JSON this should work without any problem but we should make sure that
-        // the massage is not chunked or otherwise damaged.
-        try {
-            var json = JSON.parse(message.data);
-        } catch (e) {
-            console.log('This doesn\'t look like a valid JSON: ', message.data);
+        scrollBottom();
+    }
+
+    function buildHTML(message, who) {
+        return `<div class="${'message-wrapper ' + who}">
+            <div class="circle-wrapper animated bounceIn">A</div>
+            <div class="text-wrapper animated fadeIn">${ message.text }</div>
+        </div>`;
+    }
+
+    function send(message) {
+        if (validate(message.text)) {
+            message.time = new Date().getTime();
+
+            connection.send(JSON.stringify(message));
+            if (myName) {
+                onSend(message);
+            } else {
+                myName = message.text;
+            }
+        }
+    }
+
+    function receive(message) {
+        console.log(message)
+        if (validate(message.text)) {
+            onRecieve(message);
+        }
+    }
+
+    function validate(input) {
+        return input.length;
+    }
+
+    function sendMessage() {
+        let text = input.val();
+        send({type: "text", text: text});
+
+        input.val('');
+        input.focus();
+    }
+
+    function bindEvents() {
+        input.focus();
+
+        sendButton.on('click', function(e) {
+            sendMessage();
+        });
+
+        input.on('keydown', function(e) {
+            let key = e.which || e.keyCode;
+
+            if (key === 13) { // enter key
+                e.preventDefault();
+
+                sendMessage();
+            }
+        });
+    }
+
+    function init() {
+        window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+        if (!window.WebSocket) {
+            onRecieve({type: "text", text: "Este navegador no soporta web sockets"}, "admin");
             return;
         }
 
-        // NOTE: if you're not sure about the JSON structure
-        // check the server source code above
-        if (json.type === 'color') { // first response from the server with user's color
-            myColor = json.data;
-            status.text(myName + ': ').css('color', myColor);
-            input.removeAttr('disabled').focus();
-            // from now user can start sending messages
-        } else if (json.type === 'history') { // entire message history
-            // insert every single message to the chat window
-            for (var i=0; i < json.data.length; i++) {
-                handleMessage(json.data[i]);
-            }
-        } else {
-            handleMessage(json.data)
-            input.removeAttr('disabled');
-        }
-    };
+        connection = new WebSocket('ws://127.0.0.1:1337');
 
-    /**
-     * Send mesage when user presses Enter key
-     */
-    input.keydown(function(e) {
-        if (e.keyCode === 13) {
-            var msg = $(this).val();
-            if (!msg) {
+        connection.onopen = function () {
+            onRecieve({type: "text", text: 'Escribe tu nombre:'}, "admin");
+        };
+
+        connection.onerror = function (error) {
+            onRecieve({type: "text", text: 'Error de conexión'}, "admin");
+        };
+
+        connection.onmessage = function (message) {
+
+            let json;
+            try {
+                json = JSON.parse(message.data);
+            } catch (e) {
+                console.log('This doesn\'t look like a valid JSON: ', message.data);
                 return;
             }
-            // send the message as an ordinary text
-            connection.send(JSON.stringify({type: "text", text: msg}));
-            $(this).val('');
-            // disable the input field to make the user wait until server
-            // sends back response
-            input.attr('disabled', 'disabled');
 
-            // we know that the first message sent from a user their name
-            if (myName === false) {
-                myName = msg;
+            if (json.type === 'history') {
+                for (let i=0; i < json.data.length; i++) {
+                    receive(json.data[i]);
+                }
+            } else {
+                receive(json.data);
             }
-        }
-    });
+        };
 
-    /**
-     * This method is optional. If the server wasn't able to respond to the
-     * in 3 seconds then show some error message to notify the user that
-     * something is wrong.
-     */
-    setInterval(function() {
-        if (connection.readyState !== 1) {
-            status.text('Error');
-            input.attr('disabled', 'disabled').val('Unable to comminucate '
-                                                 + 'with the WebSocket server.');
-        }
-    }, 3000);
+        bindEvents();
 
-    function handleMessage(message) {
-        const color = message.color;
-        const author = message.author;
-        const dt = new Date(message.time);
-        console.log(message)
-
-        if (message.type === 'text') {
-            content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-                + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
-                + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-                + ': ' + message.text + ' <button onclick="synthesis.speak(\''+message.text+'\')">LEER</button></p>');
-        } else if (message.type === 'image') {
-            content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-                + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
-                + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-                + ': ' + '<img height="100" src="' + message.url + '" /></p>');
-        } else if (message.type === 'audio') {
-            content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-                + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
-                + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-                + ': ' + '<audio controls src="' + message.url + '" /></p>');
-        } else if (message.type === 'speech') {
-            content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-                + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
-                + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-                + ': SPEECH: ' + message.text + ' <button onclick="synthesis.speak(\''+message.text+'\')">LEER</button></p>');
-        } else {
-            console.log('Hmm..., I\'ve never seen message like this: ', message);
-        }
+        let conectionError = false;
+        setInterval(function() {
+            if (connection.readyState !== 1) {
+                if (!conectionError) onRecieve({type: "text", text: 'Error de conexión'}, "admin");
+                conectionError = true;
+            } else {
+                conectionError = false;
+            }
+        }, 3000);
     }
-});
+
+    init();
+
+    return {
+        send,
+        receive
+    }
+})();
+
+
+
+function handleMessage(message) {
+    const color = message.color;
+    const author = message.author;
+    const dt = new Date(message.time);
+
+    if (message.type === 'text') {
+        content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+            + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
+            + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
+            + ': ' + message.text + ' <button onclick="synthesis.speak(\''+message.text+'\')">LEER</button></p>');
+    } else if (message.type === 'image') {
+        content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+            + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
+            + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
+            + ': ' + '<img height="100" src="' + message.url + '" /></p>');
+    } else if (message.type === 'audio') {
+        content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+            + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
+            + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
+            + ': ' + '<audio controls src="' + message.url + '" /></p>');
+    } else if (message.type === 'speech') {
+        content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+            + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
+            + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
+            + ': SPEECH: ' + message.text + ' <button onclick="synthesis.speak(\''+message.text+'\')">LEER</button></p>');
+    } else {
+        console.log('Hmm..., I\'ve never seen message like this: ', message);
+    }
+}
--- public/wasm-example.js
+++ public/wasm-example.js
@@ -1,39 +1,56 @@
 function filter(imageData) {
     const bufferPointerIn = 1024,
         {data, width, height} = imageData,
-        bufferIn = new Uint8Array(width * height * 4),
+        bufferIn = new Uint8Array(wasmModule.memory.buffer, bufferPointerIn, width * height * 4),
         bufferPointerOut = 2048 + width * height * 4,
-        bufferOut = new Uint8Array(width * height * 4);
+        bufferOut = new Uint8Array(wasmModule.memory.buffer, bufferPointerOut, width * height * 4);
 
-    console.log(imageData)
-    //bufferIn.set(data);
-    Module._outline_c(bufferPointerIn, bufferPointerOut, width, height);
+    bufferIn.set(data);
+    wasmModule.outline_c(bufferPointerIn, bufferPointerOut, width, height);
     data.set(bufferOut);
     return data;
 }
 
-const video = document.querySelector('#video-wasm')
 function renderSource(source, destination) {
-    source.getContext('2d').drawImage(video, 0, 0, 640, 480);
-    source.toBlob(function(blob) {
-        destination.getContext('2d').putImageData(filter(blob), 0, 0);
-    })
+    const context = destination.getContext('2d');
+    context.drawImage(source, 0, 0, destination.width, destination.height);
+
+    const imageData = context.getImageData(0, 0, destination.width, destination.height);
+    imageData.data.set(filter(imageData));
+    context.putImageData(imageData, 0, 0);
+
     requestAnimationFrame(_ => renderSource(source, destination));
 }
 
-function main() {
-
-    navigator.mediaDevices.getUserMedia({
-        video: true
-    })
-        .then(function(stream) {
-            video.srcObject = stream;
-        })
-        .catch(function(error) {
-            console.log('error', error);
+async function loadWasm() {
+    const response = await fetch('/public/wasm-filter.wasm'),
+        wasmFile = await response.arrayBuffer(),
+        compiledModule = await WebAssembly.compile(wasmFile),
+        wasmModule = await WebAssembly.instantiate(compiledModule, {
+            env: {
+                random: max => Math.floor(Math.random() * max),
+                logInt: console.log
+            }
         });
 
-    const image = document.querySelector('#canvas-wasm-2');
-    const canvas1 = document.querySelector('#canvas-wasm');
-    requestAnimationFrame(_ => renderSource(canvas1, image));
+    return wasmModule.exports;
 }
+
+async function main() {
+    const video = document.createElement('video'),
+        stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+
+    wasmModule = await loadWasm();
+
+    video.setAttribute(`width`, 640);
+    video.setAttribute(`height`, 480);
+    video.setAttribute(`src`, URL.createObjectURL(stream));
+    video.play();
+
+    const image = document.querySelector('#image');
+    requestAnimationFrame(_ => renderSource(video, image));
+}
+
+let wasmModule;
+// main()
+//     .catch(console.error);
